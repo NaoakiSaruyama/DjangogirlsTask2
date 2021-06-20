8629bebad804f9c5479c137789a635cc03206362
NaoakiSaruyama <saruyanamaoaki3412@gmail.com>
2021-06-20 13:13:45 +0900

    change part

--- blog/views.py
+++ blog/views.py
@@ -38,5 +38,6 @@ def post_edit(request,pk):
       return redirect('post_detail',pk=post.pk)
   else:
     form = PostForm(instance=post)
+
   return render(request,'blog/post_edit.html',{'form':form})
 
